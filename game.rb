ecacb12f9957f92c9be4b5e65ce6a9e26780aefe
esrig <joshua.esrig@gmail.com>
2022-07-01 15:34:29 -0700

    added game prompting and input output recongition. Added Board.is_winner? that verifies if a row or a column is a winner

--- game.rb
+++ game.rb
@@ -11,11 +11,51 @@ class Board
             puts
         end
     end
+    def modify_board(player_num, row, col)
+        @Matrix[row][col] = player_num
+    end
+    def is_winner?
+        @Matrix.each_with_index do |x, index|
+            #check if any rows are winners
+            if x[1] == x[2] && x[2] == x[3] && x[1] != ""
+                return true
+            end
+            if @Matrix[1][index] == @Matrix[2][index] &&  @Matrix[2][index] == @Matrix[3][index] && @Matrix[3][index] != ""
+                return true
+            end
+        end
 
 end
-#class Player(player_num)
-    
-#end
+class Player
+    def initialize(player_num, board)
+        @player_num = player_num
+        @board = board
+    end
+    def get_input
+        puts "Player #{@player_num}, enter which row you would like"
+        @row = gets.chomp.to_i
+        puts "Player #{@player_num}, enter which column you would like"
+        @col = gets.chomp.to_i
+        @board.modify_board(@player_num, @row, @col)
+    end 
+end
 
 test = Board.new
 test.print_board
+p1 = Player.new(1, test)
+p2 = Player.new(2, test)
+while true do
+    p1.get_input
+    test.print_board
+    if test.is_winner? == true
+        puts "Player 1 Wins"
+        break
+    end
+    p2.get_input
+    test.print_board
+    if test.is_winner? == true
+        puts "Player 2 Wins"
+        break
+    end
+end
+end
